b63173d39494136c3a0e183a3cad2941fc1168d5
= <garnaat.ben@gmail.com>
2017-01-21 15:01:13 -0800

    add routes

--- pyramid_portfolio/routes.py
+++ pyramid_portfolio/routes.py
@@ -1,3 +1,16 @@
+"""Configuration of app routes."""
+
+
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
+    # HTML Routes:
     config.add_route('home', '/')
+    config.add_route('login', '/login')
+    config.add_route('logout', '/logout')
+    config.add_route('create', '/project/create')
+    config.add_route('detail', '/project/{id:\d+}')
+    config.add_route('edit', '/project/{id:\d+}/edit')
+    config.add_route('delete', '/project/{id:\d+}/delete')
+    # API Routes:
+    config.add_route('api_get_project_all', '/api/project/all')
+    config.add_route('api_get_project_by_id', '/api/project/{id:\d+}')
