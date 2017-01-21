6573b11edd34f22041f8d7ca947b4f8c07d147c9
= <garnaat.ben@gmail.com>
2017-01-20 20:47:37 -0800

    initial scaffold

---
+++ README.txt
@@ -0,0 +1,14 @@
+pyramid_portfolio README
+==================
+
+Getting Started
+---------------
+
+- cd <directory containing this file>
+
+- $VENV/bin/pip install -e .
+
+- $VENV/bin/initialize_pyramid_portfolio_db development.ini
+
+- $VENV/bin/pserve development.ini
+
---
+++ setup.py
@@ -0,0 +1,64 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'passlib',
+    'psycopg2',
+    'pyramid',
+    'pyramid_jinja2',
+    'pyramid_debugtoolbar',
+    'pyramid_tm',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+    'waitress',
+    ]
+
+tests_require = [
+    'WebTest >= 1.3.1',  # py3 compat
+    'pytest',  # includes virtualenv
+    'pytest-cov',
+    'tox'
+    ]
+
+dev_requires = [
+    'ipython',
+    'pyramid_ipython'
+]
+
+setup(name='pyramid_portfolio',
+      version='0.0',
+      description='pyramid_portfolio',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+          "Programming Language :: Python",
+          "Framework :: Pyramid",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+      ],
+      author='Ben Garnaat',
+      author_email='garnaat.ben@gmail.com',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      extras_require={
+          'dev': dev_requires,
+          'testing': tests_require,
+      },
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = pyramid_portfolio:main
+      [console_scripts]
+      initialize_db = pyramid_portfolio.scripts.initializedb:main
+      """,
+      )
