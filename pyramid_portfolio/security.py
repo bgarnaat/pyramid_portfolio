d25be6ff76960af3f117e58c569ee8534b7d1dc1
= <garnaat.ben@gmail.com>
2017-01-21 16:51:30 -0800

    set up security config

--- pyramid_portfolio/security.py
+++ pyramid_portfolio/security.py
@@ -6,7 +6,8 @@ from pyramid.session import SignedCookieSessionFactory
 from passlib.apps import custom_app_context as pwd_context
 
 
-class NewRoot(object):
+class RootFactory(object):
+    """Root Factory object for ACL policies."""
     def __init__(self, request):
         self.request = request
 
@@ -21,3 +22,18 @@ def check_credentials(username, password):
         if username == os.environ.get('AUTH_USERNAME'):
             return pwd_context.verify(password, 'AUTH_PASSWORD')
     return False
+
+
+def includeme(config):
+    """Pyramid security configuration."""
+    auth_secret = os.environ.get('AUTH_SECRET')
+    authn_policy = AuthTkTAuthentication(secret=auth_secret, hashalg='sha512')
+    authz_policy = ACLAuthorizationPolicy()
+    config.set_authentication_policy(authn_policy)
+    config.set_authorization_policy(authz_policy)
+    config.set_root_factory(RootFactory)
+    # CSRF Protection:
+    session_secret = os.environ.get('SESSION_SECRET')
+    session_factory = SignedCookieSessionFactory(session_secret)
+    config.set_session_factory(session_factory)
+    config.set_default_csrf_options(require_csrf=True)
